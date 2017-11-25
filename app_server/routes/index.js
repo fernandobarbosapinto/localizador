d7f7f798f2f3d8c95091446d54e4567c3452cd7a
fernandobarbosapinto <fernando.mcsa@gmail.com>
2017-11-25 12:02:10 -0200

    Criando a extrutura estática do nosso localizador

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/other');
 
-/*var homepageController = function(req, res, next) {
-  res.render('index', { title: 'Express' });
-};*/
+/* Locations pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-var ctrlMain = require('../controllers/main');
-
-/* GET home page. */
-//router.get('/', homepageController); 
-
-router.get('/', ctrlMain.index);
+/* Other Pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
